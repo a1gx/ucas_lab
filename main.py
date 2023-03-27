40affcfad0bb8dab63f672ab70c083b498034b00
a1gx <1657748706@qq.com>
2023-03-27 15:06:38 +0800

    添加网卡选择功能

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sys
 from sniffer import Ui_Dialog
 from PyQt5.QtWidgets import QDialog, QApplication
+import netifaces
+
+
+class MyWindow(QDialog):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        _ui = Ui_Dialog()
+        _ui.setupUi(self)
+        self.setWindowTitle("Sniffer by A1gxer")
+        interfaces = netifaces.interfaces()
+        for _ in interfaces:
+            _ui.eth_comboBox.addItem(_)
+        _ui.eth_comboBox.setCurrentIndex(-1)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    MainWindow = QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
+    my = MyWindow()
+    my.show()
     sys.exit(app.exec_())
