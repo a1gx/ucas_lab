c033deac40a618db726a0a7546725d4c83f36705
a1gx <1657748706@qq.com>
2023-03-27 18:20:14 +0800

    定义开始与结束抓包函数

--- main.py
+++ main.py
@@ -7,15 +7,28 @@ import netifaces
 class MyWindow(QDialog):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
-        _ui = Ui_Dialog()
-        _ui.setupUi(self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
         self.setWindowTitle("Sniffer by A1gxer")
         interfaces = netifaces.interfaces()
         for _ in interfaces:
-            _ui.eth_comboBox.addItem(_)
-        _ui.eth_comboBox.setCurrentIndex(-1)
-        _ui.stop_btn.setEnabled(False)
-        _ui.begin_btn.setAutoDefault(False)
+            self.ui.eth_comboBox.addItem(_)
+        self.ui.eth_comboBox.setCurrentIndex(-1)
+        self.ui.stop_btn.setEnabled(False)
+        self.ui.begin_btn.setAutoDefault(False)
+
+        # 链接信号与槽函数
+        self.ui.begin_btn.clicked.connect(self.start)
+        self.ui.stop_btn.clicked.connect(self.stop)
+
+    # 实现基础抓包
+    def start(self):
+        print("开始执行抓包分析")
+        self.ui.stop_btn.setEnabled(True)
+
+    def stop(self):
+        print("停止执行抓包")
+        self.ui.stop_btn.setEnabled(False)
 
 
 if __name__ == '__main__':
